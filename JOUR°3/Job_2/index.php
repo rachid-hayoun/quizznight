eb13b6a28d33294a63ce3178aaea1fa187f7a067
Rachid Hayoun <rachid.hayoun@laplateforme.io>
2025-02-11 17:48:34 +0100

    Delete JOUR°3 directory

--- JOUR°3/Job_2/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-$str = ['Tous',' ces','  instants','seront',' perdus',' dans',' le',' temps',' comme',' les',' larmes',' sous', ' la',' pluie.'];
-
-echo $str[0] . $str[2] . $str[4] . $str[6] . $str[8] . $str[10] . $str[12];
--- JOUR°3/Job_4/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$str = "Dans l'espace, personne ne vous entend crier";
-
-// Afficher le nombre de caractères
-echo "Le nombre de caractères dans la chaîne est : " . strlen($str);
